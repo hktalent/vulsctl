c44e1eb5c2b23a2fe5a4ba46b175c3e696d20bd0
51pwn <18223385+hktalent@users.noreply.github.com>
2022-04-17 20:06:48 +0800

    up

--- docker/oval.sh
+++ docker/oval.sh
@@ -77,6 +77,17 @@ case "$target" in
 		-v $PWD:/goval-dictionary \
 		vuls/goval-dictionary fetch suse --suse-type opensuse-leap 15.3
 		;;
+	--all) docker run --rm -it ${DOCKER_NETWORK_OPT} -v $PWD:/goval-dictionary vuls/goval-dictionary fetch redhat ${@} 6 7 8
+		docker run --rm -it ${DOCKER_NETWORK_OPT} -v $PWD:/goval-dictionary vuls/goval-dictionary fetch amazon ${@}
+		docker run --rm -it ${DOCKER_NETWORK_OPT} -v $PWD:/goval-dictionary vuls/goval-dictionary fetch debian ${@} 9 10 11
+		docker run --rm -it ${DOCKER_NETWORK_OPT} -v $PWD:/goval-dictionary vuls/goval-dictionary fetch ubuntu ${@} 14 16 18 20
+		docker run --rm -it ${DOCKER_NETWORK_OPT} -v $PWD:/goval-dictionary vuls/goval-dictionary fetch alpine ${@} 3.12 3.13 3.14 3.15
+		docker run --rm -it ${DOCKER_NETWORK_OPT} -v $PWD:/goval-dictionary vuls/goval-dictionary fetch oracle ${@}
+		docker run --rm -it ${DOCKER_NETWORK_OPT} -v $PWD:/goval-dictionary vuls/goval-dictionary fetch fedora ${@} 34 35
+		docker run --rm -it ${DOCKER_NETWORK_OPT} -v $PWD:/goval-dictionary vuls/goval-dictionary fetch suse --suse-type suse-enterprise-server ${@} 12 15
+		docker run --rm -it ${DOCKER_NETWORK_OPT} -v $PWD:/goval-dictionary vuls/goval-dictionary fetch suse --suse-type opensuse tumbleweed
+		docker run --rm -it ${DOCKER_NETWORK_OPT} -v $PWD:/goval-dictionary vuls/goval-dictionary fetch suse --suse-type opensuse-leap 15.3
+		;;
 	--*)  echo "specify [--redhat --amazon --debian --ubuntu --alpine --oracle --fedora]"
 		exit 1
 		;;
